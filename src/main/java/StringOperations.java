ec38d13c80d485c50e9de6a341a3a66609c47528
sata21 <tssrohith1@gmail.com>
2022-04-23 22:54:44 +0200

    tests rt 2

--- src/main/java/StringOperations.java
+++ src/main/java/StringOperations.java
@@ -9,7 +9,7 @@ public class StringOperations {
         int length = original.length();
         for (int i = length - 1; i >= 0; i--)
             reverse = reverse + original.charAt(i);
-        return reverse;
+        return null;
     }
 
     //StringConcatenation
