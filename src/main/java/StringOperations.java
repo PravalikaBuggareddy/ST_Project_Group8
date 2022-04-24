9c38c01611c581dc5a1e6ca8a932aa143caec11a
trayi <trayivannu@gmail.com>
2022-04-24 23:36:51 +0200

    pass test case by ibrahim

--- src/main/java/StringOperations.java
+++ src/main/java/StringOperations.java
@@ -57,7 +57,7 @@ public class StringOperations {
         for(int i=ch.length-1;i>=0;i--){
             rev+=ch[i];
         }
-        return null;
+        return rev;
     }
 
 
@@ -67,14 +67,62 @@ public class StringOperations {
     //Description: This function is used to merge two strings
     public static String StringConcatenation(String s1, String s2){
         String s3 = s1.concat(s2);
-        return null;
+        return s1;
     }
     //Author: Rohith
     //Description: This function is used to give the length of string
     public String StringLength(String s1){
         int length = s1.length();
-        return null;
+        return String.valueOf(length);
     }
+
+    // Author :Ibrahim
+    // Description : The below method returns the number of vowels in the string
+    //  This function is tested by Unit Test : test_count1Vowels()
+    public int countVowels(String S1)
+    {
+        int count = 0;
+
+        char[] Chars = S1.toCharArray();
+
+        for (char chr : Chars)
+        {
+            if (chr == 'a' || chr == 'A' || chr == 'e' || chr == 'E' || chr == 'i' || chr == 'I' || chr =='o' || chr == 'O' || chr == 'u' || chr == 'U' )
+            {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    // Author :Ibrahim
+    // Description : The below method returns the number of consonants in the string
+    //  This function is tested by Unit Test : test_countConsonants()
+    public int countConsonants(String S2)
+    {
+        int count = 0;
+
+        char[] Chars = S2.toCharArray();
+
+        for ( char chr: Chars)
+        {
+            if (chr != 'a' && chr != 'A' && chr != 'e' && chr != 'E' && chr != 'i' && chr != 'I' && chr != 'o' && chr != 'O' && chr != 'u' && chr != 'U')
+            {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+
+    // Author :Ibrahim
+    // Description : The below method returns the number of lowercase in the string
+    //  This function is tested by Unit Test : test_StringLowercase()
+    public String String_Lowercase(String Str3)
+    {
+        String Str4 = Str3.toLowerCase();
+        return Str4;
+    }
+
 
 
 }
--- src/test/java/String_OperationsTest.java
+++ src/test/java/String_OperationsTest.java
@@ -53,6 +53,31 @@ public class String_OperationsTest {
     public void testlengthstring(){
         assertEquals("15", StringOperation.StringLength("SoftwareTesting"));
     }
+
+    //Author :Ibrahim
+    //Description: This test tests the String_Lowercase() method
+    @Test
+    public void test_String_Lowercase()
+    {
+        assertEquals("refer" , StringOperation.String_Lowercase("REFER"));
+    }
+
+    //Author :Ibrahim
+    //Description: This test tests the countConsonants() method
+    @Test
+    public void test_countConsonants()
+    {
+        assertEquals(4,StringOperation.countConsonants("Ibrahim"));
+    }
+
+    //Author :Ibrahim
+    //Description: This test tests the test_countVowels method
+    @Test
+    public void test_countVowels()
+    {
+        assertEquals(3,StringOperation.countVowels("Ibrahim"));
+    }
+
 }
 
 
