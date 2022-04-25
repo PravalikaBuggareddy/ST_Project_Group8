ff25db9ce35394c1713b5dc33836e7cfbcf333f1
Pravalika Buggareddy <pravallikabuggareddy@gmail.com>
2022-04-25 20:11:37 +0200

    Pass test case by Pravalika

--- src/main/java/StringOperations.java
+++ src/main/java/StringOperations.java
@@ -12,21 +12,20 @@ public class StringOperations {
         }
         return rev;
 
-
     }
 
     //Author: Rohith
     //Description: This function is to merge two strings
     public static String StringConcatenation(String s1, String s2) {
         String s3 = s1.concat(s2);
-        return s1;
+        return s3;
     }
 
     //Author: Rohith
     //Author: This function is used to give length of the string
     public String StringLength(String s1) {
         int length = s1.length();
-        return String.valueOf(length);
+        return length;
     }
 
     // Author :Ibrahim
@@ -64,7 +63,7 @@ public class StringOperations {
                 count = count + 1;
             }
         }
-        return String.valueOf(count);
+        return count;
     }
 
 
@@ -76,28 +75,52 @@ public class StringOperations {
         return Str4;
     }
 
-    //Author: Pravalika
-    //Description: The below method returns the count of occurence of a letter
+    // Author: Pravalika
+    // Description: This below method returns the count of occurence of a letter
     // This function is tested by Unit Test : test_StringOccurence()
-    public static int StringOccurence(String word){
+
+    public static int StringOccurence(String Word) {
         char Character = 'a';
-        for(int i=0; i<word.length();i++){
-            count++;
+        int count = 0;
+        for (int i = 0; i < Word.length(); i++) {
+            if (Word.charAt(i) == Character) {
+                count++;
+            }
         }
         return count;
+
     }
 
-    //Author : Pravalika
-    // Description: The below method removes the extra spaces at the starting and ending of the string
+    // Author: Pravalika
+    // Description: This below method removes the extra spaces at the starting and ending of the string
+    // This function is tested by Unit Test : test_StringTrim()
+    public String StringTrim(String word) {
+        word = word.trim();
+        return word;
+    }
+
+    // Author: Pravalika
+    // Description: This below method returns the boolean value by checking whether a string is anagram or not
     // This function is tested by Unit Test : test_isAnagram()
-     boolean isAnagram(String str1, String str2){
-        String s1 = str1.replaceAll("\\s","");
-        String s2 = str2.replaceAll("\\s","");
-        s1.toLowerCase();
-        s2.toLowerCase();
-        flag = Arrays.equals(s1,s2);
-        return flag;
-     }
+    public boolean isAnagram(String str1, String str2) {
+        String s1 = str1.replaceAll("\\s", "");
+        String s2 = str1.replaceAll("\\s", "");
+        boolean flag = true;
+        if (s1.length() != s2.length()){
+            flag = false;
+        }
+        else{
+            char[] ArrayS1 = s1.toLowerCase().toCharArray();
+            char[] ArrayS2 = s2.toLowerCase().toCharArray();
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            flag = Arrays.equals(ArrayS1, ArrayS2);
+        }
+        if(flag)
+            return true;
+        else
+            return false;
+    }
 }
 
 
