e152fe734587bb24119ba3cf6c3d421cec2dbe27
Pravalika Buggareddy <pravallikabuggareddy@gmail.com>
2022-04-25 15:56:51 +0200

    fail test cases by Pravalika

--- src/main/java/StringOperations.java
+++ src/main/java/StringOperations.java
@@ -1,131 +1,22 @@
-import java.util.Arrays;
-import java.util.Locale;
-
 public class StringOperations {
     //String Operations
-    // Author: Pravalika
-    // Description: This below method returns the count of occurence of a letter
-    // This function is tested by Unit Test : test_StringOccurence()
 
-    public static int StringOccurence(String Word) {
-        char Character = 'a';
-        int count = 0;
-        for (int i = 0; i < Word.length(); i++) {
-            if (Word.charAt(i) == Character) {
-                count++;
-            }
-        }
+    // Author: Pravalika
+    // Description: This method removes the extra spaces at the starting and ending of the string
+    // This function is tested by Unit Test: test_StringTrim()
+    public String StringTrim(String word){
+        return null;
+    }
+
+    //Author: Pravalika
+    //Description: This method returns the count of occurence of a letter
+    // This function is tested by Unit Test: test_StringOccurence()
+    public static int StringOccurence(String word){
         return 0;
 
     }
 
-    // Author: Pravalika
-    // Description: This below method removes the extra spaces at the starting and ending of the string
-    // This function is tested by Unit Test : test_StringTrim()
-    public String StringTrim(String word) {
-        word = word.trim();
-        return word;
+    public boolean isAnagram(String str1, String str2){
+        return false;
     }
-
-    // Author: Pravalika
-    // Description: This below method returns the boolean value by checking whether a string is anagram or not
-    // This function is tested by Unit Test : test_isAnagram()
-    public boolean isAnagram(String str1, String str2) {
-        String s1 = str1.replaceAll("\\s", "");
-        String s2 = str1.replaceAll("\\s", "");
-        boolean flag = true;
-        if (s1.length() != s2.length()){
-            flag = false;
-        }
-        else{
-            char[] ArrayS1 = s1.toLowerCase().toCharArray();
-            char[] ArrayS2 = s2.toLowerCase().toCharArray();
-            Arrays.sort(ArrayS1);
-            Arrays.sort(ArrayS2);
-            flag = Arrays.equals(ArrayS1, ArrayS2);
-        }
-        if(flag)
-            return true;
-        else
-            return false;
-    }
-    //Author: Rohith
-    //Description: This function is used to reverse the given string
-    public static String reverseString(String str){
-        char ch[]=str.toCharArray();
-        String rev="";
-        for(int i=ch.length-1;i>=0;i--){
-            rev+=ch[i];
-        }
-        return null;
-    }
-
-
-
-
-    //Author: Rohith
-    //Description: This function is used to merge two strings
-    public static String StringConcatenation(String s1, String s2){
-        String s3 = s1.concat(s2);
-        return s3;
-    }
-    //Author: Rohith
-    //Description: This function is used to give the length of string
-    public String StringLength(String s1){
-        int length = s1.length();
-        return String.valueOf(length);
-    }
-
-    // Author :Ibrahim
-    // Description : The below method returns the number of vowels in the string
-    //  This function is tested by Unit Test : test_count1Vowels()
-    public int countVowels(String S1)
-    {
-        int count = 0;
-
-        char[] Chars = S1.toCharArray();
-
-        for (char chr : Chars)
-        {
-            if (chr == 'a' || chr == 'A' || chr == 'e' || chr == 'E' || chr == 'i' || chr == 'I' || chr =='o' || chr == 'O' || chr == 'u' || chr == 'U' )
-            {
-                count = count + 1;
-            }
-        }
-        return count;
-    }
-
-    // Author :Ibrahim
-    // Description : The below method returns the number of consonants in the string
-    //  This function is tested by Unit Test : test_countConsonants()
-    public int countConsonants(String S2)
-    {
-        int count = 0;
-
-        char[] Chars = S2.toCharArray();
-
-        for ( char chr: Chars)
-        {
-            if (chr != 'a' && chr != 'A' && chr != 'e' && chr != 'E' && chr != 'i' && chr != 'I' && chr != 'o' && chr != 'O' && chr != 'u' && chr != 'U')
-            {
-                count = count + 1;
-            }
-        }
-        return count;
-    }
-
-    // Author :Ibrahim
-    // Description : The below method returns the number of lowercase in the string
-    //  This function is tested by Unit Test : test_StringLowercase()
-    public String String_Lowercase(String Str3)
-    {
-        String Str4 = Str3.toLowerCase();
-        return Str3;
-    }
-
-
-
 }
-
-
-////
--- src/test/java/String_OperationsTest.java
+++ src/test/java/String_OperationsTest.java
@@ -11,84 +11,27 @@ public class String_OperationsTest {
         StringOperation = new StringOperations();
     }
 
-    // Author: Pravalika
-    // Description: This test tests the StringOccurence() function
+    //Author: Pravalika
+    //Description: This test tests the StringTrim() method
 
     @Test
-    public void test_StringOccurence() {
-        assertEquals(3, StringOperations.StringOccurence("Pravallika"));
+    public void test_StringTrim(){
+        assertEquals("madam", StringOperation.StringTrim("  madam "));
     }
 
-    // Author: Pravalika
-    // Description: This test tests the StringTrim() function
+    //Author: Pravalika
+    //Description: This test tests the StringOccurence() function
 
     @Test
-    public void test_StringTrim() {
-        assertEquals("madam", StringOperation.StringTrim("madam"));
+    public void test_StringOccurence(){
+        assertEquals(3,StringOperation.StringOccurence("Pravalika"));
     }
 
-    // Author: Pravalika
-    // Description: Ths test tests the Anagram() function
+    //Author: Pravalika
+    //Description: The test tests the Anagram() function
     @Test
-    public void test_isAnagram() {
-        assertTrue(StringOperation.isAnagram("rat", "tar"));
-    }
-
-    //Author: Rohith Tadi
-    //Description: This test case is used to test the reversing string function
-    @Test
-    public void testreversestring(){
-        assertEquals("nitin", StringOperation.reverseString("nitin"));
-    }
-
-    //Author: Rohith Tadi
-    //Description: This test case is used to test the concatenation of strings function
-    @Test
-    public void testconcatenation(){
-        assertEquals("Snow", StringOperation.StringConcatenation("Snow", "ball"));
-    }
-    //Author: Rohith Tadi
-    //Description: This test case is used to test the length of string function
-    @Test
-    public void testlengthstring(){
-        assertEquals("15", StringOperation.StringLength("SoftwareTesting"));
-    }
-
-    //Author :Ibrahim
-    //Description: This test tests the String_Lowercase() method
-    @Test
-    public void test_String_Lowercase()
-    {
-        assertEquals("refer" , StringOperation.String_Lowercase("REFER"));
-    }
-
-    //Author :Ibrahim
-    //Description: This test tests the countConsonants() method
-    @Test
-    public void test_countConsonants()
-    {
-        assertEquals(4,StringOperation.countConsonants("Ibrahim"));
-    }
-
-    //Author :Ibrahim
-    //Description: This test tests the test_countVowels method
-    @Test
-    public void test_countVowels()
-    {
-        assertEquals(3,StringOperation.countVowels("Ibrahim"));
-    }
-
-    // Author : Rohith Tadi
-    //Description: This is an integration test case for the methods countconsonants,stringtrim,stringreverse
-    @Test
-    public void RohithTest_Intergration(){ assertEquals(3, StringOperation.countConsonants(StringOperation.reverseString(StringOperation.StringTrim("level"))));
-    }
-
-    // Author :Ibrahim
-    //Description: This is an integration test for the methods StringOccurence, StringReverse, StringLowercase
-
-    @Test
-    public void Ibrahim_Integration() {assertEquals(1,StringOperation.StringOccurence(StringOperations.reverseString(StringOperation.String_Lowercase(StringOperation.StringConcatenation("RACE","car")))));
+    public void test_isAnagram(){
+        assertTrue(StringOperation.isAnagram("rat","tar"));
     }
 
 }
